07976c63f8310144612d232841c40a5294de21d6
hiro5900 <achyut5900@gmail.com>
2023-11-10 00:32:03 +0530

    Used props and multiple expense items.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { id: 'e1', title: 'Car Insurance', LocationOfExpenditure: 'Banri', amount: 294.67, date: new Date(2023, 6, 13) },
+    { id: 'e2', title: 'Fuel Refill', LocationOfExpenditure: 'Fuel Station Near Wall Mart, Banri', amount: 1.67, date: new Date(2023, 10, 8) },
+    { id: 'e3', title: 'Food', LocationOfExpenditure: 'Wall Mart Food Court, Banri', amount: 4.67, date: new Date(2023, 10, 1) },
+    { id: 'e4', title: 'Mobile Bill', LocationOfExpenditure: 'Home', amount: 2, date: new Date(2023, 10, 11) },
+  ]
+
+  const expenseItems = []
+  for(let i = 0; i < expenses.length; i++){
+    expenseItems.push(
+      <ExpenseItem title={expenses[i].title} amount={expenses[i].amount} date={expenses[i].date}/>
+    )
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Learn React.js from YouTube. Happy Learning.</p>
-        <ExpenseItem />
+        {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/> */}
+        {expenseItems}
       </header>
     </div>
   );
