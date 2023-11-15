cda6076da33a43d9a778214f642a5abffd800e98
hiro5900 <achyut5900@gmail.com>
2023-11-15 23:34:54 +0530

    Created a new button that changes the amount.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,18 @@
-import ExpenseDate from './ExpenseDate'
-import ExpenseDetails from './ExpenseDetails'
-import Card from '../UI/Card'
-import './ExpenseItem.css'
+import React, { useState } from 'react';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-     const clickHandler = () => {
-          console.log('clicked')
-     };
+     // const [titleChange, setTitleChange] = useState(props.title);
+     // const clickHandler = () => {
+     //      setTitleChange('Updated')
+     //      console.log(titleChange);
+     // };
+
      const deleteBtn = (event) => {
-          event.target.parentElement.remove()
+          event.target.parentElement.remove();
      };
      return (
           <Card className='expense-item'>
@@ -18,7 +22,7 @@ const ExpenseItem = (props) => {
                     amount={props.amount}
                     LocationOfExpenditure={props.LocationOfExpenditure}
                />
-               <button onClick={clickHandler}>Change title</button>
+               {/* <button onClick={clickHandler}>Change title</button> */}
                <button onClick={deleteBtn}>Delete</button>
           </Card>
      );
